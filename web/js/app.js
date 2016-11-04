bbe9a476bd7ca41812fa190216b6c077397fb250
brechoire <brechoire.j@gmail.com>
2016-11-04 13:14:47 +0100

    update

--- web/js/app.js
+++ web/js/app.js
@@ -16,18 +16,27 @@ $(document).ready(function(){
             $('#error').prepend('Aucun billet');
         }else
         {
-            $('#button').prepend('<a id="reset" class="btn btn-warning">Annuler</a>');
+            $('#button').prepend('<a id="reset" class="btn btn-danger">Annuler</a>');
             while (tour <= numberTicket) {
 
                 $('#bookingNext').remove();
                 $('#error').remove();
 
-                $('#info-visitor').before('<p>Informations sur le visiteur ' + tour + '</p><div class="form-group"><label for="lastname">Nom</label><input type="text" class="form-control" id="lastname"></div><div class="form-group"><label for="firstname">Prénom</label><input type="text" class="form-control" id="firstname"></div><div class="form-group"><label for="firstname">Votre pays</label><select id="" name="" class="form-control"><option value="FR" selected>France</option><option value="ZA">Belgique</option><option value="ZA">Suisse</option></select> </div> <div class="form-group"><label for="champ">Votre date de naissance</label> <input type="date" id="champ" class="form-control" name="date" placeholder="jj/mm/aaaa"></div> <div class="checkbox"><label><input type="checkbox"> Tarif réduit</label></div></div> <hr class="louvre">');
+                $('#info-visitor').prepend('<h4>Informations sur le visiteur ' + tour + '</h4><div class="form-group"><label for="lastname">Nom</label><input type="text" class="form-control" id="lastname"></div><div class="form-group"><label for="firstname">Prénom</label><input type="text" class="form-control" id="firstname"></div><div class="form-group"><label for="firstname">Votre pays</label><select id="" name="" class="form-control"><option value="FR" selected>France</option><option value="ZA">Belgique</option><option value="ZA">Suisse</option></select> </div> <div class="form-group"><label for="champ">Votre date de naissance</label> <input type="date" id="champ" class="form-control" name="date" placeholder="jj/mm/aaaa"></div> <div class="checkbox"><label><input type="checkbox"> Tarif réduit</label></div></div> <hr class="louvre">');
 
                 tour++;
 
             }
         }
+
+        $('#reset').click(function () {
+            $('#info-visitor').remove();
+            console.log('Test');
+            $('#reset').remove();
+            $('#button').prepend('<a id="bookingNext" class="btn btn-info">Suite</a>');
+        });
     });
+
+
 
 });
