cf0937f12b6f4114623347a41b92717f2e0bfd24
brechoire <brechoire.j@gmail.com>
2016-11-11 20:53:27 +0100

    add entity

---
+++ src/BookingBundle/Entity/Booking.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace BookingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Booking
+ *
+ * @ORM\Table(name="booking")
+ * @ORM\Entity(repositoryClass="BookingBundle\Repository\BookingRepository")
+ */
+class Booking
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="numero_commande", type="string", length=255)
+     */
+    private $numeroCommande;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_commande", type="date")
+     */
+    private $dateCommande;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_visite", type="date")
+     */
+    private $dateVisite;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nombre_billet", type="integer")
+     */
+    private $nombreBillet;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set numeroCommande
+     *
+     * @param string $numeroCommande
+     *
+     * @return Booking
+     */
+    public function setNumeroCommande($numeroCommande)
+    {
+        $this->numeroCommande = $numeroCommande;
+
+        return $this;
+    }
+
+    /**
+     * Get numeroCommande
+     *
+     * @return string
+     */
+    public function getNumeroCommande()
+    {
+        return $this->numeroCommande;
+    }
+
+    /**
+     * Set dateCommande
+     *
+     * @param \DateTime $dateCommande
+     *
+     * @return Booking
+     */
+    public function setDateCommande($dateCommande)
+    {
+        $this->dateCommande = $dateCommande;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCommande
+     *
+     * @return \DateTime
+     */
+    public function getDateCommande()
+    {
+        return $this->dateCommande;
+    }
+
+    /**
+     * Set dateVisite
+     *
+     * @param \DateTime $dateVisite
+     *
+     * @return Booking
+     */
+    public function setDateVisite($dateVisite)
+    {
+        $this->dateVisite = $dateVisite;
+
+        return $this;
+    }
+
+    /**
+     * Get dateVisite
+     *
+     * @return \DateTime
+     */
+    public function getDateVisite()
+    {
+        return $this->dateVisite;
+    }
+
+    /**
+     * Set nombreBillet
+     *
+     * @param integer $nombreBillet
+     *
+     * @return Booking
+     */
+    public function setNombreBillet($nombreBillet)
+    {
+        $this->nombreBillet = $nombreBillet;
+
+        return $this;
+    }
+
+    /**
+     * Get nombreBillet
+     *
+     * @return int
+     */
+    public function getNombreBillet()
+    {
+        return $this->nombreBillet;
+    }
+}
+
